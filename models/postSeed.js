cb398a179cab961c629fa9634cca5172d20994ee
Dainyl Cua <dainylcua@gmail.com>
2021-10-13 16:34:08 -0500

    Funny seeds

--- models/postSeed.js
+++ models/postSeed.js
@@ -1,4 +1,5 @@
-module.exports = [{
+module.exports = [
+    {
         "name": "Generic Post",
         "author": "61671fdbaa5acacc65af733b",
         "description": "Just a generic post for testing",
@@ -61,5 +62,122 @@ module.exports = [{
             "weight": "1",
             "value": "580gp"
         }
-    }
+    },
+    {
+        "name": "test post please ignore",
+        "author": "61674fd34f6d7cb643e3700d",
+        "description": "test post please ignore",
+        "contentType": "general",
+        "content": {
+            "name": "Kharos",
+            "description": "Don't tell me what to do!",
+            "features": "Upvoted."
+        }
+    },
+    {
+        "name": "Gun",
+        "author": "61674fff4f6d7cb643e3700f",
+        "description": "Gun",
+        "contentType": "item",
+        "content": {
+            "name": "9mm Beretta",
+            "description": "Gun",
+            "itemType": "offensive",
+            "rarity": "artifact",
+            "wondrous": true,
+            "attunement": true,
+            "damage": "2d4 piercing",
+            "effects": "Gun",
+            "weight": "4",
+            "value": "500gp"
+        }
+    },
+    {
+        "name": "My cool orange wingsuit",
+        "author": "61674f474f6d7cb643e37007",
+        "description": "THEY ARE NOT PAJAMAS",
+        "contentType": "item",
+        "content": {
+            "name": "Wingsuit",
+            "description": "Helps you commit crimes with both direction and magnitude.",
+            "itemType": "defensive",
+            "rarity": "artifact",
+            "wondrous": true,
+            "attunement": false,
+            "ac": "20",
+            "effects": "Again, THEY ARE NOT PAJAMAS! It makes you look wayyyy cooler when you put it on.",
+            "weight": "3",
+            "value": "500,000pp"
+        }
+    },
+    {
+        "name": "The best casette mixes you'll find",
+        "author": "61674f944f6d7cb643e3700b",
+        "description": "It's the last thing I have from my mother...",
+        "contentType": "item",
+        "content": {
+            "name": "Awesome Mix Vol. 1 & 2",
+            "description": "For Peter, with love. From Meredith.",
+            "itemType": "misc",
+            "rarity": "uncommon",
+            "wondrous": false,
+            "attunement": true,
+            "effects": "Plays sick tunes when put into a casette player.",
+            "weight": "0.5",
+            "value": "1sp"
+        }
+    },
+    {
+        "name": "Laser Sword",
+        "author": "616750224f6d7cb643e37011",
+        "description": "Just like in the old Jedi stories.",
+        "contentType": "item",
+        "content": {
+            "name": "Lightsaber",
+            "description": "Make sure it's pointing the right direction.",
+            "itemType": "offensive",
+            "rarity": "artifact",
+            "wondrous": true,
+            "attunement": true,
+            "damage": "2d8 radiant",
+            "effects": "Only wieldable by the those chosen by the Force.",
+            "weight": "8",
+            "value": "1pp"
+        }
+    },
+    {
+        "name": "Need to open any lock? Use this!",
+        "author": "61674f784f6d7cb643e37009",
+        "description": "Only for those who are strong of heart!",
+        "contentType": "item",
+        "content": {
+            "name": "Keyblade",
+            "description": "Not oficially licensed by Disney.",
+            "itemType": "offensive",
+            "rarity": "artifact",
+            "wondrous": true,
+            "attunement": true,
+            "damage": "3d6 bludgeoning",
+            "effects": "Allows the wielder attuned to the item access the 'Wielder' spell list.",
+            "weight": "5",
+            "value": "500,000pp"
+        }
+    },
+    {
+        "name": "My homemade Butterscotch Pie. alexa stop",
+        "author": "616750324f6d7cb643e37013",
+        "description": "Also known as ButtsPie",
+        "contentType": "item",
+        "content": {
+            "name": "Butterscotch Pie",
+            "description": "The smell reminds you of something...",
+            "itemType": "misc",
+            "rarity": "common",
+            "wondrous": false,
+            "attunement": false,
+            "effects": "Fully restores the user's HP",
+            "weight": "1",
+            "value": "180gp"
+        }
+    },
 ]
